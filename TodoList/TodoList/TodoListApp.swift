ae6cb0e4029ced46b1cbc7a0ad3846e5e4fe362f
Gourab Choudhuri <74147934+gourab99-tech@users.noreply.github.com>
2024-01-24 17:31:40 +0530

    Initial commit

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Choudhuri, Gourab on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
