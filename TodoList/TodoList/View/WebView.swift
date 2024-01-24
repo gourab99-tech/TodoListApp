1d184f111ce9f5692769bab0209beea3301a5d02
Gourab Choudhuri <74147934+gourab99-tech@users.noreply.github.com>
2024-01-25 00:22:07 +0530

    News feature added

---
+++ TodoList/TodoList/View/WebView.swift
@@ -0,0 +1,43 @@
+//
+//  WebView.swift
+//  TodoList
+//
+//  Created by Choudhuri, Gourab on 25/01/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+    }
+}
+
+
+#Preview {
+    WebView(urlString: "")
+}
